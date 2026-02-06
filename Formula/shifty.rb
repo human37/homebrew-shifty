68e823ae72b5646b74592bd6c1023736347c83cc
Ammon Taylor <ammonx9@gmail.com>
2026-02-06 11:46:28 -0700

    shifty v0.0.6

--- Formula/shifty.rb
+++ Formula/shifty.rb
@@ -1,8 +1,8 @@
 class Shifty < Formula
   desc "Menu bar posture shifter for macOS"
   homepage "https://github.com/human37/shifty"
-  url "https://github.com/human37/shifty/archive/refs/tags/v0.0.5.tar.gz"
-  sha256 "6568bcf0b3ccb052c26d25df813bf0f762a60dcecb0736fffd6c996f9ebca23a"
+  url "https://github.com/human37/shifty/archive/refs/tags/v0.0.6.tar.gz"
+  sha256 "691d514144274e3deb38501ca6f846a368b0e9dfebffb659cf9d83795d362d3a"
   head "https://github.com/human37/shifty.git", branch: "main"
 
   depends_on :macos
